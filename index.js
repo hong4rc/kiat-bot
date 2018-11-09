38a1d7b0f1619e4b923ec6a4d141771f9d2f9e97
Ｈｏｎｇａｒｃ <anhhong12121997t@gmail.com>
2018-11-10 02:10:20 +0700

    Un-sleep

--- index.js
+++ index.js
@@ -19,22 +19,23 @@ if (user) {
     // login with  cookie file
     user = {appState: JSON.parse(fs.readFileSync('state.json', 'utf8'))};
 }
-const TIME_OUT_MSG = 30000;
-const MY_TIME_ZONE = 7;
-const HOUR_IN_DAY = 24;
-const MIN_SEND_SLEEP = 3600000;
 
-const timeSleep = {
-    start: 1,
-    end: 5
-};
+// const TIME_OUT_MSG = 30000;
+// const MY_TIME_ZONE = 7;
+// const HOUR_IN_DAY = 24;
+// const MIN_SEND_SLEEP = 3600000;
 
-const getHour = () => (new Date().getUTCHours() + MY_TIME_ZONE) % HOUR_IN_DAY;
+// const timeSleep = {
+//     start: 1,
+//     end: 5
+// };
+
+// const getHour = () => (new Date().getUTCHours() + MY_TIME_ZONE) % HOUR_IN_DAY;
 login(user)
     .then(api => {
         log.setApi(api);
 
-        const listSent = {};
+        // const listSent = {};
 
         // fs.writeFileSync('state.json', JSON.stringify(api.getAppState()));
         api.listen((err, msg) => {
@@ -45,17 +46,17 @@ login(user)
                 case 'presence':
                     log.info(msg.userId, msg.statUser ? 'online' : 'idle');
 
-                    if (msg.statUser && !listSent[msg.userId]) {
-                        const nowHour = getHour();
-                        if (nowHour >= timeSleep.start && nowHour <= timeSleep.end) {
-                            api.sendMessage('good night', msg.userId)
-                                .then(msgId => {
-                                    setTimeout(() => api.deleteMessage([msgId]), TIME_OUT_MSG);
-                                    listSent[msg.userId] = true;
-                                    setTimeout(() => delete listSent[msg.userId], MIN_SEND_SLEEP);
-                                });
-                        }
-                    }
+                    // if (msg.statUser && !listSent[msg.userId]) {
+                    //     const nowHour = getHour();
+                    //     if (nowHour >= timeSleep.start && nowHour <= timeSleep.end) {
+                    //         api.sendMessage('good night', msg.userId)
+                    //             .then(msgId => {
+                    //                 setTimeout(() => api.deleteMessage([msgId]), TIME_OUT_MSG);
+                    //                 listSent[msg.userId] = true;
+                    //                 setTimeout(() => delete listSent[msg.userId], MIN_SEND_SLEEP);
+                    //             });
+                    //     }
+                    // }
                     break;
                 case 'typ':
                     api.sendTyping(msg.from, msg.isTyping);
