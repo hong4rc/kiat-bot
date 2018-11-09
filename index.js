00a7f4eee889c34882aa203929d78ffe903c1887
Ｈｏｎｇａｒｃ <anhhong12121997t@gmail.com>
2018-11-09 23:01:41 +0700

    Good night :)

--- index.js
+++ index.js
@@ -20,43 +20,46 @@ if (user) {
     user = {appState: JSON.parse(fs.readFileSync('state.json', 'utf8'))};
 }
 const TIME_OUT_MSG = 30000;
+const MY_TIME_ZONE = 7;
+const HOUR_IN_DAY = 24;
+const MIN_SEND_SLEEP = 3600000;
+
+const timeSleep = {
+    start: 1,
+    end: 5
+};
+
+const getHour = () => (new Date().getUTCHours() + MY_TIME_ZONE) % HOUR_IN_DAY;
 login(user)
     .then(api => {
         log.setApi(api);
 
+        const listSent = {};
+
         // fs.writeFileSync('state.json', JSON.stringify(api.getAppState()));
-        const stopper = {};
         api.listen((err, msg) => {
             if (err) {
                 log.error(err);
             }
-            let from;
             switch (msg.type) {
                 case 'presence':
                     log.info(msg.userId, msg.statUser ? 'online' : 'idle');
 
-                    // if (msg.statUser) {
-                    //     let nowHour = timer.getCurrentTime().getHours();
-                    //     if (nowHour >= 1 && nowHour <= 3) {
-                    //         api.sendMessage('Chào bạn buổi sáng tốt lành, chúc ngủ ngon !!!', msg.userId);
-                    //     }
-                    // }
-                    break;
-                case 'typ':
-                    from = msg.from;
-                    if (msg.isTyping) {
-                        log.info(`${from} is typing.`);
-                        stopper[from] = api.sendTyping(from);
-                        setTimeout(() => {
-                            delete stopper[from];
-                        }, TIME_OUT_MSG);
-
-                    } else {
-                        log.info(`${from} is not typing.`);
-                        if (stopper[from]) {
-                            stopper[from]();
+                    if (msg.statUser && !listSent[msg.userId]) {
+                        const nowHour = getHour();
+                        if (nowHour >= timeSleep.start && nowHour <= timeSleep.end) {
+                            api.sendMessage('good night', msg.userId)
+                                .then(msgId => {
+                                    setTimeout(() => api.deleteMessage([msgId]), TIME_OUT_MSG);
+                                    listSent[msg.userId] = true;
+                                    setTimeout(() => delete listSent[msg.userId], MIN_SEND_SLEEP);
+                                });
                         }
                     }
+                    break;
+                case 'typ':
+                    api.sendTyping(msg.from, msg.isTyping);
+                    log.info(`${msg.from} is ${msg.isTyping ? '' : 'not'} typing.`);
                     break;
                 case 'message':
                     api.markAsRead(msg.threadId);
