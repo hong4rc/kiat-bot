de46fb6fbb1028c123c28f0082e74d3a9c80a94e
Hongarc <anhhong12121997t@gmail.com>
2019-03-31 13:24:37 +0700

    migrate node-facebook@0.5.1-rc3

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
-const fs = require('fs');
 const log = require('kiat-log');
-const login = require('node-facebook');
+const Facebook = require('node-facebook');
 const chat = require('./chat');
 
 const timer = require('./timer');
@@ -10,66 +9,34 @@ const delta = 5;
 
 // let user = {email: 'your username/id', pass: 'your pass'};
 
-let user = process.env.user;
+const state = JSON.parse(process.env.user);
 
-if (user) {
-    user = JSON.parse(user);
-    if (!user.email || !user.pass) {
+const me = new Facebook({state});
+(async user => {
+    const api = await user.login();
+    api.listen();
 
-        // login with cookie from .env
-        user = {appState: user};
-    }
-} else {
+    let nextSeconds;
+    const timerBio = () => {
+        timer.tick();
+        nextSeconds = timer.getNext();
+        api.changeBio(`${timer.getTime()}\nNước sông chảy cạn,\ncá tự bơi đi chỗ khác.....,`, nextSeconds + delta)
+            .then(() => setTimeout(timerBio, nextSeconds * milis));
+    };
+    timerBio();
 
-    // login with  cookie file
-    user = {appState: JSON.parse(fs.readFileSync('state.json', 'utf8'))};
-}
-
-login(user)
-    .then(api => {
-        log.setApi(api);
-
-        let nextSeconds;
-        const timerBio = () => {
-            timer.tick();
-            nextSeconds = timer.getNext();
-            api.changeBio(`${timer.getTime()}\nNước sông chảy cạn,\ncá tự bơi đi chỗ khác.....,`, nextSeconds + delta)
-                .then(() => setTimeout(timerBio, nextSeconds * milis));
-        };
-        timerBio();
-
-
-        // fs.writeFileSync('state.json', JSON.stringify(api.getAppState()));
-        api.listen((err, msg) => {
-            if (err) {
-                log.error(err);
-            }
-            const source = msg.senderId || msg.from;
-            if (source === api.getCurrentUserId()) {
-
-                // use some command
-                return;
-            }
-            switch (msg.type) {
-                case 'presence':
-                    log.info(msg.userId, msg.statUser ? 'online' : 'idle');
-
-                    break;
-                case 'typ':
-                    api.sendTyping(msg.from, msg.isTyping);
-                    log.info(`${msg.from} is ${msg.isTyping ? '' : 'not'} typing.`);
-                    break;
-                case 'message':
-                    api.markAsRead(msg.threadId);
-                    if (msg.threadId === msg.senderId) {
-                        chat(msg.body, msg.threadId)
-                            .then(res => res && api.sendMessage(res, msg.threadId), log.error);
-                    }
-                    break;
-                default:
-
-                // log.info(msg);
-
-            }
-        });
+    api.on('presence', msg => {
+        log.info(msg.userId, msg.statUser ? 'online' : 'idle');
     });
+    api.on('typ', msg => {
+        api.sendTyping(msg.from, msg.isTyping);
+        log.info(`${msg.from} is ${msg.isTyping ? '' : 'not'} typing.`);
+    });
+    api.on('msg', async msg => {
+        api.markAsRead(msg.threadId);
+        if (msg.threadId === msg.senderId) {
+            const data = await chat(msg.body, msg.threadId);
+            api.sendMsg({body: data}, msg.threadId);
+        }
+    });
+})(me);
