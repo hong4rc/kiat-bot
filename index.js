ead704c047bf36260ab1191b60e97e2e5c240c08
Hongarc <anhhong12121997t@gmail.com>
2019-05-08 20:30:28 +0700

    Update eslint rule and lint code

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-'use strict';
+
 const log = require('kiat-log');
 const Facebook = require('node-facebook');
 const chat = require('./chat');
 
 const timer = require('./timer');
+
 const milis = 1000;
 const delta = 5;
 
@@ -11,32 +12,32 @@ const delta = 5;
 
 const state = JSON.parse(process.env.user);
 
-const me = new Facebook({state});
-(async user => {
-    const api = await user.login();
-    api.listen();
+const me = new Facebook({ state });
+(async (user) => {
+  const api = await user.login();
+  api.listen();
 
-    let nextSeconds;
-    const timerBio = () => {
-        timer.tick();
-        nextSeconds = timer.getNext();
-        api.changeBio(`${timer.getTime()}\nNước sông chảy cạn,\ncá tự bơi đi chỗ khác.....,`, nextSeconds + delta)
-            .then(() => setTimeout(timerBio, nextSeconds * milis));
-    };
-    timerBio();
+  let nextSeconds;
+  const timerBio = () => {
+    timer.tick();
+    nextSeconds = timer.getNext();
+    api.changeBio(`${timer.getTime()}\nNước sông chảy cạn,\ncá tự bơi đi chỗ khác :)`, nextSeconds + delta)
+      .then(() => setTimeout(timerBio, nextSeconds * milis));
+  };
+  timerBio();
 
-    api.on('presence', msg => {
-        log.info(msg.userId, msg.statUser ? 'online' : 'idle');
-    });
-    api.on('typ', msg => {
-        api.sendTyping(msg.from, msg.isTyping);
-        log.info(`${msg.from} is ${msg.isTyping ? '' : 'not'} typing.`);
-    });
-    api.on('msg', async msg => {
-        api.markAsRead(msg.threadId);
-        if (msg.threadId === msg.senderId) {
-            const data = await chat(msg.body, msg.threadId);
-            api.sendMsg({body: data}, msg.threadId);
-        }
-    });
+  api.on('presence', (message) => {
+    log.info(message.userId, message.statUser ? 'online' : 'idle');
+  });
+  api.on('typ', (message) => {
+    api.sendTyping(message.from, message.isTyping);
+    log.info(`${message.from} is ${message.isTyping ? '' : 'not'} typing.`);
+  });
+  api.on('msg', async (message) => {
+    api.markAsRead(message.threadId);
+    if (message.threadId === message.senderId) {
+      const data = await chat(message.body, message.threadId);
+      api.sendMsg({ body: data }, message.threadId);
+    }
+  });
 })(me);
